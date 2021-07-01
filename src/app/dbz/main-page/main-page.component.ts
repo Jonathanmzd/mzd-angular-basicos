13e06a563199cd9e146b35c2e0bd210c4915d1a4
Jonathan Ardila <mized2009@gmail.com>
2021-07-01 09:48:06 -0500

    65. Centralizar el acceso de los personajes en el servicio

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -4,32 +4,16 @@ import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html'
+  templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
-
+  
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
-    poder: 1000
+    poder: 1000,
   }
 
-  agregarNuevoPersonaje(argumento: Personaje){
-    this.personajes.push(argumento);
-  }
-
-  constructor (private dbzService: DbzService){}
+  constructor() {}
 }
-
 
 // Usando F5 podemos iniciar los run debug en visual code, ademas podemos realizar un point o marcado en la linea que deseamos realizar que sucede en esa line ade codigo que variables, ETC.
