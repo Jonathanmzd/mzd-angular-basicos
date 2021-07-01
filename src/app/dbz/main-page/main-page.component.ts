21da97cef5edddf947988b74feb27b81fbb76ec9
Jonathan Ardila <mized2009@gmail.com>
2021-06-30 21:26:07 -0500

    Mostrar listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 interface Personaje {
   nombre: string;
-  poder : number;
+  poder: number;
 }
 
 @Component({
@@ -11,17 +11,32 @@ interface Personaje {
 })
 export class MainPageComponent {
 
-  nuevo:Personaje = {
-    nombre:'Trucks',
-    poder: 14000
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
   }
 
-  cambiarNombre(event:any){
-    console.log(event.target.value);
-  }
-  
-  agregar(){
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
     console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre:'',
+      poder: 0
+    }
   }
 
 }
