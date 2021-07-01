aa2a1d8433cd10ca60e8f458bf379931484298f1
Jonathan Ardila <mized2009@gmail.com>
2021-07-01 09:13:12 -0500

    @Outputs y EventEmitter

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,29 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-agregar',
-  templateUrl: './agregar.component.html'
+  templateUrl: './agregar.component.html',
 })
 export class AgregarComponent {
-
-  @Input() personajes:Personaje[] = [];
-
   @Input() nuevo: Personaje = {
     nombre: '',
-    poder: 0
-  }
+    poder: 0,
+  };
+
+  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
 
   agregar() {
     if (this.nuevo.nombre.trim().length === 0) {
       return;
     }
-    console.log(this.nuevo);
-    this.personajes.push(this.nuevo);
+    // console.log(this.nuevo);
+    this.onNuevoPersonaje.emit(this.nuevo);
     this.nuevo = {
-      nombre:'',
-      poder: 0
-    }
+      nombre: '',
+      poder: 0,
+    };
   }
-
 }
+
+// ¿Qué es @Input en angular?
+// Los inputs nos permiten pasar valores desde el componente «padre» que utiliza dentro del el otro componente y así intercambiar datos entre estos dos componentes. ... Con esto ya sabemos usar los input en Angular 2 para interactuar y pasar datos entre componentes.
+
+// ¿Qué es @Output en angular?
+// Un patrón común en Angular es compartir datos entre un componente principal y uno o más componentes secundarios. ... @Input () permite que un componente principal actualice los datos del componente secundario. Por el contrario, @Output () permite que el hijo envíe datos a un componente principal
